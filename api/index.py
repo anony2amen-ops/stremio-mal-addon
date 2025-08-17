83d6451ec1e843816545eb7506929efccb3d2e0c
anony2amen-ops <anony2amen@gmail.com>
2025-08-17 11:05:14 +0530

    "Test: Ultra minimal Python handler"

--- api/index.py
+++ api/index.py
@@ -1,34 +1,9 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse, HTMLResponse
+from http.server import BaseHTTPRequestHandler
 
-# Create FastAPI app
-app = FastAPI(title="Malayalam Movies Test")
-
-@app.get("/")
-async def root():
-    """Simple test endpoint"""
-    return HTMLResponse(content="""
-    <html>
-    <body>
-        <h1>🎬 Malayalam Movies Addon - TEST</h1>
-        <p>If you see this, FastAPI is working!</p>
-        <a href="/manifest.json">Test Manifest</a>
-    </body>
-    </html>
-    """)
-
-@app.get("/manifest.json")  
-async def manifest():
-    """Simple manifest test"""
-    return JSONResponse({
-        "id": "test.malayalam.addon",
-        "version": "1.0.0", 
-        "name": "Malayalam Test",
-        "description": "Test addon",
-        "resources": ["catalog"],
-        "types": ["movie"],
-        "catalogs": [{"type": "movie", "id": "test", "name": "Test"}]
-    })
-
-# CRITICAL: Export handler for Vercel
-handler = app
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'<h1>ULTRA MINIMAL TEST - IT WORKS!</h1>')
+        return
